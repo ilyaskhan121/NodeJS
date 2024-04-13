b4bcc2d5adda2debfa17b7e4c11b8a0e2c5439f6
Mac <mac@Macs-MacBook-Pro.local>
2024-04-13 12:27:22 +0500

    first app

---
+++ app.js
@@ -0,0 +1,5 @@
+const second = require("./appUtilits");//importing our modules using require function
+console.log(second.sum(3,3));
+console.log(second.PI);
+console.log(new second.Student());
+console.log(second.myDateTime())
